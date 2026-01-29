2a1ade095869f8c7a6987d30f0aa8bb90187b75e
Sabu Siyad <hello@ssiyad.com>
2026-01-30 01:10:59 +0530

    feat(frontend): fetch attempts from backend

--- frontend/src/pages/Attempts.vue
+++ frontend/src/pages/Attempts.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import { Badge, usePageMeta } from "frappe-ui";
+import { formatDate } from "date-fns";
+import { useRouter } from "vue-router";
+import { Badge, createListResource, usePageMeta } from "frappe-ui";
 import { statusTheme } from "../utils/badgeThemes";
 import { pageTitle } from "../utils/page";
 
-const attempts = [
-	{ title: "Attempt 1", status: "Pending", date: "2024-06-01" },
-	{ title: "Attempt 2", status: "Accepted", date: "2024-06-02" },
-	{ title: "Attempt 3", status: "Rejected", date: "2024-06-03" },
-	{ title: "Attempt 4", status: "Accepted", date: "2024-06-04" },
-	{ title: "Attempt 5", status: "Pending", date: "2024-06-05" },
-];
+const router = useRouter();
+
+const attemptsResource = createListResource({
+	doctype: "Bounty Attempt",
+	cache: ["Attempts"],
+	fields: ["name", "title", "creation", "status"],
+	pageLength: 99999,
+	auto: true,
+});
 
 usePageMeta(() => ({
 	title: pageTitle("Attempts"),
@@ -17,16 +21,21 @@ usePageMeta(() => ({
 </script>
 
 <template>
-	<div class="container mx-auto max-w-3xl py-16">
+	<div class="container mx-auto py-16">
 		<div class="divide-y">
 			<div class="flex pb-4 font-medium">
 				<div class="w-1/2">Title</div>
 				<div class="w-1/4 text-end">Date</div>
 				<div class="w-1/4 text-end">Status</div>
 			</div>
-			<div v-for="attempt in attempts" :key="attempt.title" class="flex py-4 cursor-pointer">
+			<div
+				v-for="attempt in attemptsResource.data"
+				:key="attempt.name"
+				class="flex py-4 cursor-pointer"
+				@click="router.push({ name: 'Attempt', params: { id: attempt.name } })"
+			>
 				<div class="w-1/2">{{ attempt.title }}</div>
-				<div class="w-1/4 text-end">{{ attempt.date }}</div>
+				<div class="w-1/4 text-end">{{ formatDate(attempt.creation, "PPP") }}</div>
 				<div class="w-1/4 text-end">
 					<Badge :label="attempt.status" :theme="statusTheme(attempt.status)" />
 				</div>
