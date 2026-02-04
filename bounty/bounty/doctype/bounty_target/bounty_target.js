134c48743efeef6193ae8bf0223ebb084047bc71
Sabu Siyad <hello@ssiyad.com>
2026-02-04 15:37:27 +0530

    feat: git clone sources

--- bounty/bounty/doctype/bounty_target/bounty_target.js
+++ bounty/bounty/doctype/bounty_target/bounty_target.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, Sabu Siyad and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bounty Target", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Bounty Target", {
+	refresh(frm) {
+		[["↓", "Get Code", "get_code"]].forEach(([icon, label, action]) => {
+			frm.add_custom_button(icon + " " + __(label), () => {
+				frm.call(action).then(() => {
+					frm.refresh();
+				});
+			});
+		});
+	},
+});
--- bounty/bounty/doctype/bounty_target/bounty_target.py
+++ bounty/bounty/doctype/bounty_target/bounty_target.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2026, Sabu Siyad and contributors
 # For license information, please see license.txt
 
-# import frappe
+import os
+import pathlib
+import git
+
+import frappe
+import frappe.utils
 from frappe.model.document import Document
 
 
 class BountyTarget(Document):
-	pass
+	@property
+	def base_path(self):
+		return (
+			pathlib.Path(frappe.utils.get_bench_path())
+			.joinpath("sentinel")
+			.joinpath("sources")
+			.as_posix()
+		)
+
+	@property
+	def source_path(self):
+		return pathlib.Path(self.base_path).joinpath(self.name).as_posix()
+
+	@frappe.whitelist()
+	def	get_code(self):
+		self.clean_up()
+		git.Repo.clone_from(self.repository, self.source_path)
+
+	def clean_up(self):
+		if os.path.exists(self.source_path):
+			os.rmdir(self.source_path)
