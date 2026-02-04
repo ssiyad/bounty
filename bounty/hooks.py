5f3c5ff2a95df6b32eb75dfea8a3c22a090ef52b
Sabu Siyad <hello@ssiyad.com>
2026-02-04 20:49:48 +0530

    fix: reroute rules

--- bounty/hooks.py
+++ bounty/hooks.py
@@ -5,6 +5,10 @@ app_description = "Manage confidential reports, issues and rewards"
 app_email = "hello@ssiyad.com"
 app_license = "gpl-3.0"
 
+website_route_rules = [
+	{"from_route": "/b/<path:app_path>", "to_route": "b"},
+]
+
 permission_query_conditions = {
 	"Bounty Hunter": "bounty.bounty.doctype.bounty_hunter.bounty_hunter.permission_query",
 }
