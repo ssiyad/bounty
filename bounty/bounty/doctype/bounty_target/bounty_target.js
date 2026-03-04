ef9486e77a37c1ee6aa0cd7702f662bdf49e2e66
Sabu Siyad <hello@ssiyad.com>
2026-03-04 23:38:34 +0530

    chore: cleanup bounty target

--- bounty/bounty/doctype/bounty_target/bounty_target.js
+++ bounty/bounty/doctype/bounty_target/bounty_target.js
@@ -1,18 +1,8 @@
 // Copyright (c) 2026, Sabu Siyad and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Bounty Target", {
-	refresh(frm) {
-		[
-			["Sync Code", "sync_code"],
-			["Index Logs", "index_logs"],
-			["Index Code", "index_code"],
-		].forEach(([label, action]) => {
-			frm.add_custom_button(__(label), () => {
-				frm.call(action).then(() => {
-					frm.refresh();
-				});
-			});
-		});
-	},
-});
+// frappe.ui.form.on("Bounty Target", {
+// 	refresh(frm) {
+
+// 	},
+// });
