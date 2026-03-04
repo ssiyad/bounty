04e8e6e81cc876b2717fd1ed2e7b115880eaa648
Sabu Siyad <hello@ssiyad.com>
2026-03-04 23:27:55 +0530

    chore: remove sherlock
    bybye!

--- bounty/bounty/doctype/sentinel_job/sentinel_job.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Sabu Siyad and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Sentinel Job", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- bounty/sherlock/doctype/sherlock_python_class/sherlock_python_class.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Sabu Siyad and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Sherlock Python Class", {
-// 	refresh(frm) {
-
-// 	},
-// });
