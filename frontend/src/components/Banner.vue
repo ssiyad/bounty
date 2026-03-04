2482379e32951e02516c5717edc367708616b6a9
Sabu Siyad <hello@ssiyad.com>
2026-03-04 20:44:41 +0530

    fix: banner x padding

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -12,7 +12,7 @@ withDefaults(
 
 <template>
 	<div
-		class="px-4 py-3 border-b"
+		class="px-5 py-3 border-b"
 		:class="{
 			'bg-surface-gray-1 text-ink-gray-9': variant === 'default',
 			'bg-surface-blue-1 text-ink-blue-3': variant === 'info',
