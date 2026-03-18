b6df7613cad93a90fe4258b30847fc514c9ee20f
Sabu Siyad <hello@ssiyad.com>
2026-03-18 17:16:56 +0530

    fix(topbar): padding and height

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -13,7 +13,7 @@ withDefaults(
 </script>
 
 <template>
-	<div class="h-12.5 px-4 py-3 flex items-center justify-between border-b">
+	<div class="h-12 shrink-0 px-5 flex items-center justify-between border-b">
 		<Breadcrumbs :items="breadcrumbs" />
 		<div class="flex items-center gap-2">
 			<div id="topbar-actions" />
