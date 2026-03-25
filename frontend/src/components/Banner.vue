849fcf1f1ad244d71c32527ccdb6bf07381d1e4c
Sabu Siyad <hello@ssiyad.com>
2026-03-25 23:28:24 +0530

    fix(banner): use fixed height

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -12,7 +12,7 @@ withDefaults(
 
 <template>
 	<div
-		class="px-5 py-3 border-b flex items-center justify-between"
+		class="h-12 px-5 border-b flex items-center justify-between"
 		:class="{
 			'bg-surface-gray-1 text-ink-gray-9': variant === 'default',
 			'bg-surface-blue-1 text-ink-blue-3': variant === 'info',
