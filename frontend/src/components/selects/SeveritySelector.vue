157498a4d98439b4257fd2296f173a6ab91076c7
Sabu Siyad <hello@ssiyad.com>
2026-03-26 19:11:04 +0530

    feat(frontend): severity selector: show color on trigger

--- frontend/src/components/selects/SeveritySelector.vue
+++ frontend/src/components/selects/SeveritySelector.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { Select, SelectProps, createResource } from "frappe-ui";
 
 const model = defineModel<string>();
@@ -9,7 +10,10 @@ const severities = createResource({
 	url: "security.api.severity.get_severities",
 	auto: true,
 	cache: ["severities"],
+	initialData: [],
 });
+
+const current = computed(() => severities.data.find((s: any) => s.value === model.value));
 </script>
 
 <template>
@@ -20,6 +24,18 @@ const severities = createResource({
 		placeholder="Severity"
 		:options="severities.data"
 	>
+		<template v-if="current" #prefix>
+			<div
+				class="size-2 rounded-full"
+				:class="{
+					'bg-surface-gray-5': current?.color == 'gray',
+					'bg-surface-red-5': current?.color == 'red',
+					'bg-red-400': current?.color == 'orange',
+					'bg-surface-green-3': current?.color == 'green',
+					'bg-surface-blue-3': current?.color == 'blue',
+				}"
+			/>
+		</template>
 		<template #option="{ option }">
 			<div class="inline-flex gap-2 items-center">
 				<div
@@ -31,7 +47,7 @@ const severities = createResource({
 						'bg-surface-green-3': option.color == 'green',
 						'bg-surface-blue-3': option.color == 'blue',
 					}"
-				></div>
+				/>
 				{{ option.label }}
 			</div>
 		</template>
